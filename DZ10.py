bd99e6190bd94982fc8bc94bbb3b9fc1b0de2a8e
Alex Sosic <alexsosici@yahoo.com>
2020-11-11 22:01:53 +0100

    zadaca_datoteke

---
+++ DZ10.py
@@ -0,0 +1,6 @@
+with open("Tablica.csv", "r", encoding="UTF8") as tablica_file:
+    contents=tablica_file.read().splitlines()
+
+    for row in contents:
+        row_data = row.split(",")
+        print(f"{row_data[0]} is {row_data[1]} years old and {row_data[2]}")
